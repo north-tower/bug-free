c68da0cebb60e123c1db4440121b991232538941
north-tower <muneshmyke@gmail.com>
2024-05-18 16:27:37 +0300

    23

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -21,7 +21,7 @@ function Budget() {
           Budgets
         </h1>
         <div className="bg-white py-2 px-3">
-          <nav className="flex flex-wrap gap-4">
+          {/* <nav className="flex flex-wrap gap-4">
           
             <Popover>
               <PopoverTrigger>Add Budget</PopoverTrigger>
@@ -51,27 +51,9 @@ function Budget() {
               </div>
               </PopoverContent>
             </Popover> 
-          </nav>
+          </nav> */}
         </div>
-        <Table>
-          <TableCaption>A list of your budgets.</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Budgets</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell className="font-medium">INV001</TableCell>
-      <TableCell>Overdue</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell className="text-right">$250.00</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
+        
         </div>
         
       </div>
