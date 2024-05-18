03c1e3764a440edbbd6c91f8f00180ea4239dd7a
north-tower <muneshmyke@gmail.com>
2024-05-18 16:19:14 +0300

    ft

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -21,10 +21,18 @@ interface Expense {
   category: string;
 }
 
+interface Category {
+  id: string;
+  name: string;
+  budget: string;
+}
+
 
 function Orders() {
 
   const [newExpense, setNewExpense] = useState<Expense>({id: '', description: '', amount: '' , category: ''});
+  const [newCategory, setNewCategory] = useState<Category>({id: '', name: '', budget: ''});
+
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -37,10 +45,31 @@ function Orders() {
     }));
   };
 
+  const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewCategory(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
       await axios.post('https://supreme-goggles-beta.vercel.app/api/v1/addDriver', newExpense);
+      // Display success message
+      window.alert('Expense added successfully');
+      // Redirect to Drivers page
+      window.location.reload();
+   
+    } catch (error) {
+      console.error('Error adding new driver:', error);
+    }
+  };
+
+  const handleCategory = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      await axios.post('https://supreme-goggles-beta.vercel.app/api/v1/addCategory', newCategory);
       // Display success message
       window.alert('Expense added successfully');
       // Redirect to Drivers page
@@ -136,19 +165,30 @@ function Orders() {
               <PopoverContent>
               <p className="mt-4 pl-4 text-xl font-bold">Add Category</p>
               <div className="flex flex-col items-center px-8 py-10">
+              <form onSubmit={handleCategory}>
                   <label className="block w-full">
                     <p className="mb-1 text-sm text-gray-600">Enter Category Name</p>
-                    <input className="w-full rounded-md border bg-white py-2 px-2 outline-none ring-blue-600 focus:ring-1" type="text" placeholder="Enter Category Name" />
+                    <input className="w-full rounded-md border bg-white py-2 px-2 outline-none ring-blue-600 focus:ring-1"
+                       id="name"
+                       name="name"
+                       value={newCategory.name}
+                       onChange={handleCategoryChange}
+                    type="text" placeholder="Enter Category Name" />
                   </label>
                   <label className="block w-full">
                     <p className="mb-1 text-sm text-gray-600">Enter Category Budget</p>
-                    <input className="w-full rounded-md border bg-white py-2 px-2 outline-none ring-blue-600 focus:ring-1" type="text" placeholder="Enter Category Budget" />
+                    <input className="w-full rounded-md border bg-white py-2 px-2 outline-none ring-blue-600 focus:ring-1"
+                     id="budget"
+                     name="budget"
+                     value={newCategory.budget}
+                     onChange={handleCategoryChange}
+                    type="text" placeholder="Enter Category Budget" />
                   </label>
                  
                   <div className="mt-8 flex flex-col justify-center space-y-3 sm:flex-row sm:space-x-3 sm:space-y-0">
                   <button className="whitespace-nowrap rounded-md bg-blue-500 px-4 py-3 font-medium text-white">Add Category</button>
-                
                 </div>
+                </form>
 
               </div>
               </PopoverContent>
