1b76348334014fc830383c72a05068d03d6049ee
north-tower <muneshmyke@gmail.com>
2024-05-20 17:34:48 +0300

    lo

--- app/orders/columns.tsx
+++ app/orders/columns.tsx
@@ -46,20 +46,20 @@ export const columns: ColumnDef<Payment>[] = [
     accessorKey: "category",
     header: "Category",
   },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Email
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      )
-    },
-  },
+  // {
+  //   accessorKey: "email",
+  //   header: ({ column }) => {
+  //     return (
+  //       <Button
+  //         variant="ghost"
+  //         onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+  //       >
+  //         Email
+  //         <ArrowUpDown className="ml-2 h-4 w-4" />
+  //       </Button>
+  //     )
+  //   },
+  // },
   {
     accessorKey: "amount",
     header: () => <div className="text-right">Amount</div>,
--- app/orders/data-table.tsx
+++ app/orders/data-table.tsx
@@ -76,10 +76,10 @@ export function DataTable<TData, TValue>({
     <div>
         <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter status..."
+          value={(table.getColumn("status")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("status")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
